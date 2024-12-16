c967cbf6092adf256981730788a0d9ffc1fa3819
prdy14 <118160201+prdy14@users.noreply.github.com>
2024-12-16 07:40:38 +0530

    added feactures of cart

--- script.js
+++ script.js
@@ -7532,13 +7532,17 @@ const product = document.getElementById("products");
 const favList = JSON.parse(localStorage.getItem("favList")) || [];
 const cartList = JSON.parse(localStorage.getItem("cartList")) || [];
 
+const count = document.getElementById("count");
+count.innerText = cartList.length;
 result.map((ele, i) => {
   const card = document.createElement("div");
   card.className = "m-3";
   card.innerHTML = `
     <div class="bg-white flex flex-col text-slate-700 rounded-xl sm:rounded-2xl shadow-md group">
           <div class="grid">
-            <div class="bg-primary text-xs py-1 pl-2  rounded-lg font-semibold text-white z-10 layer h-6 w-16 m-4">21%
+            <div class="bg-primary text-xs py-1 pl-2  rounded-lg font-semibold text-white z-10 layer h-6 w-16 m-4">${Math.floor(
+              (110 / ele.price.value) * 100
+            )}%
               off
             </div>
             <div class="layer mr-4 ml-auto mt-4 sm:group-hover:opacity-100 sm:opacity-0 z-10 duration-500 ">
@@ -7568,9 +7572,20 @@ result.map((ele, i) => {
                 }</p>
                 <del>$${ele.price.value + 100}</del>
               </div>
-              <button class="border-2 rounded-md pr-1 grid border-primary" id="cart${i}">
-                <div class="layer w-0.5 h-3 bg-primary mp"></div>
-                <div class="layer w-3 h-0.5 bg-primary ml"></div>
+              <button class="border-2 rounded-md grid ${
+                isThere(cartList, ele) !== -1
+                  ? ` border-slate-600`
+                  : `border-primary`
+              }" id="cart${i}">
+                ${
+                  isThere(cartList, ele) == -1
+                    ? `<svg class="w-6" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AddIcon">
+                  <path fill="#d23f57" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"></path>
+                </svg>`
+                    : `<svg class="w-6" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="RemoveIcon">
+                  <path d="M19 13H5v-2h14z"></path>
+                </svg>`
+                }
               </button>
             </div>
           </div>
@@ -7595,17 +7610,91 @@ result.map((ele, i) => {
   });
 
   const cart = document.getElementById(`cart${i}`);
-  console.log(cart);
+
   cart.addEventListener("click", () => {
     const there = isThere(cartList, ele);
     console.log(there);
     if (there == -1) {
       cartList.push(ele);
+      cart.classList.remove("border-primary");
+      cart.classList.add("border-slate-600");
+      cart.innerHTML = `<svg class="w-6" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="RemoveIcon">
+                  <path d="M19 13H5v-2h14z"></path>
+                </svg>`;
+    } else {
+      cartList.splice(there, 1);
+      cart.classList.remove("border-slate-600");
+      cart.classList.add("border-primary");
+      cart.innerHTML = `<svg class="w-6" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AddIcon">
+                  <path fill="#d23f57" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"></path>
+                </svg>`;
     }
-    cart.innerHTML = `<a class="ml-1 text-primary font-semibold" href="/cart">go to cart</a>`;
+    count.innerText = cartList.length;
     localStorage.setItem("cartList", JSON.stringify(cartList));
   });
 });
+
+const show = document.getElementById("cart");
+const itemsCount = document.getElementById("items");
+function openCart() {
+  cartProducts.innerHTML = "";
+  itemsCount.innerText = `${cartList.length} items`;
+  show.style.display = "flex";
+  let sum = 0;
+  cartList.forEach((ele, i) => {
+    sum = sum + ele.price.value;
+    const product = document.createElement("div");
+    product.className = "flex mt-6 w-full justify-between";
+    product.innerHTML = `
+ <div class="flex mt-6 w-full justify-between">
+          <div class="w-24 h-24  bg-slate-50 rounded-2xl ">
+            <img
+              src="${ele.images[0].url}"
+              alt="" class="object-contain w-24 h-24">
+          </div>
+          <div class="w-3/5 flex flex-col text-sm">
+            <p>${ele.name} </p>
+            <p class="text-sm text-slate-600">${ele.price.value} x 1</p>
+            <div class="flex mt-4">
+              <button class="border w-5 h-5 rounded-md  flex justify-center items-center border-primary">
+                <svg class="w-4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AddIcon">
+                  <path fill="#d23f57" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"></path>
+                </svg>
+              </button>
+              <p class="text-center mx-2">1</p>
+
+              <button class="border w-5 h-5 rounded-md border-slate-600 flex justify-center items-center">
+                <svg class="w-4" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="RemoveIcon">
+                  <path d="M19 13H5v-2h14z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <button id=${ele.code}>
+            <svg class="w-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ClearIcon">
+              <path fill="#94a3b8"
+                d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+              </path>
+            </svg>
+          </button>
+        </div>
+  `;
+    cartProducts.appendChild(product);
+
+    const item = document.getElementById(`${ele.code}`);
+    item.addEventListener("click", () => {
+      cartList.splice(isThere(cartList, ele), 1);
+      localStorage.setItem("cartList", JSON.stringify(cartList));
+      openCart();
+    });
+  });
+  const totalBill = document.getElementById("checkout");
+  totalBill.innerText = `Total Bill: ${sum}`;
+}
+
+function closeCart() {
+  show.style.display = "none";
+}
 
 function isThere(arr, ele) {
   let flag = -1;
@@ -7619,6 +7708,7 @@ function isThere(arr, ele) {
 
 const searchInput = document.getElementById("search");
 const searchButton = document.getElementById("searchButton");
+const cartProducts = document.getElementById("cartProducts");
 
 searchButton.addEventListener("click", () => {
   const searchValue = searchInput.value.toLowerCase();
